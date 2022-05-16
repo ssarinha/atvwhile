9d14d20cb31a556d7ad7fd1af04d68a39996fb31
ssarinha <102757853+ssarinha@users.noreply.github.com>
2022-05-16 14:20:05 -0300

    Add files via upload

---
+++ atvwhile.js
@@ -0,0 +1,32 @@
+
+//1
+let a = 1
+while (a <= 100){
+    console.log (a);
+    a = a + 1
+}
+
+//2
+let b = 50
+while (b <= 100){
+    console.log (b);
+    b = b + 1
+}
+
+
+//3
+let d = 10
+while (d >= 0){
+    console.log (d);
+    d = d - 1
+}
+
+//4
+let c = 0
+while (c <= 20){
+    console.log (c);
+    c = c + 2
+}
+
+
+
